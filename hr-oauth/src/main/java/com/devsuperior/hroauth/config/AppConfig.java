2e3ed666881949f0493c4d66fa8a9020155ecba2
alanjxs@gmail.com <alanjxs@gmail.com>
2022-09-21 15:56:14 -0300

    Login e token JWT

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/config/AppConfig.java
@@ -0,0 +1,39 @@
+package com.devsuperior.hroauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+	
+	
+	/*
+	 * Criação de um Bean para evitar acessos não autorizado ao sistema
+	 * O BCrypt utiliza o algoritmo hash
+	 */
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	/*
+	 * Criação de Beans Jwt que é um padrão para autenticação e troca de informações.
+	 */
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MY-SECRET-KEY");//chave secreta para assinar os tokens
+		return tokenConverter;
+	}
+	
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter()); //Responsável por ler as informações do token
+	}
+	
+
+}
